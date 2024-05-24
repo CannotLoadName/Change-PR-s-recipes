0f899ffc1c49e51f50732fd3d87051e80ecbab2e
CannotLoadName <electricfan@yeah.net>
2024-05-24 20:40:24 +0800

    项目文件

--- main.py
+++ main.py
@@ -1,69 +1,53 @@
-#-*-coding:utf-8;-*-
-from os import listdir
-from os.path import dirname,isdir,splitext,join
 from sys import argv
-from json import dumps,loads
-path=dirname(__file__)
-def addpth(a,b):
-    return join(a,b)
-def todic(j):
-    for i in range(0,len(j)):
-        j[i]=loads(j[i])
-    return j
-fla=open(addpth(path,'original.csv'),'r')
-flb=open(addpth(path,'replacement.csv'),'r')
-flc=open(addpth(path,'blacklist.csv'),'r')
-fld=open(addpth(path,'declinematerials.csv'),'r')
-ori=todic(fla.read().strip('\n').split('\n'))
-repl=todic(flb.read().strip('\n').split('\n'))
-blist=flc.read().strip('\n').split('\n')
-decm=fld.read().strip('\n').split('\n')
-fla.close()
-flb.close()
-flc.close()
-fld.close()
-def decline(dc):
-    m=' '+dc[1][0]+' '
-    dc[0]=m
-    dc[2]=m
-    return dc
-def gettzm(nm):
-    ret=splitext(nm)[1]
-    if ret[0]=='.':
-        ret=ret[1:]
-    return ret
-def repldic(dic):
-    global ori,repl
-    for i in range(0,len(ori)):
-        if dic==ori[i]:
-            dic=repl[i]
-            break
-    return dic
-def repldics(dics):
-    for i in dics:
-        dics[i]=repldic(dics[i])
-    return dics
-def dealfile(fn,sgn):
-    f=open(fn,'r')
-    finc=loads(f.read())
-    f.close()
-    if finc.__contains__('key'):
-        finc['key']=repldics(finc['key'])
-    if sgn and finc.__contains__('pattern'):
-        finc['pattern']=decline(finc['pattern'])
-    f=open(fn,'w')
-    f.write(dumps(finc,indent=2))
-    f.close()
-def dealdir(fp,sign):
-    global blist,decm
-    for f in listdir(fp):
-        fn=addpth(fp,f)
-        if isdir(fn):
-            dealdir(fn,sign or f=='recipes')
-        else:
-            if sign and gettzm(f)=='json' and not(f in blist):
-                dealfile(fn,f in decm)
-if len(argv)>=2:
-    dealdir(argv[1],False)
-else:
-    dealdir(input('输入模组文件解压到的目录：'),False)
+from os import listdir
+from os.path import join, dirname, splitext, isdir
+from json import loads, dumps
+
+path = dirname(__file__)
+blacklist = open(join(path, "blacklist.csv"), "r").read().strip("\r\n").split("\n")
+original = open(join(path, "original.csv"), "r").read().strip("\r\n").split("\n")
+for i in range(len(original)):
+    original[i] = loads(original[i])
+replacement = open(join(path, "replacement.csv"), "r").read().strip("\r\n").split("\n")
+for i in range(len(replacement)):
+    replacement[i] = loads(replacement[i])
+declinematerials = open(join(path, "declinematerials.csv"), "r").read().strip("\r\n").split("\n")
+sawmaterials = open(join(path, "sawmaterials.csv"), "r").read().strip("\r\n").split("\n")
+changeresult = loads(open(join(path, "changeresult.json"), "r").read())
+
+
+def handle(dname, fname):
+    if splitext(fname)[1] == ".json":
+        fd = loads(open(join(dname, fname), "r").read())
+        if fname in changeresult:
+            fd["result"] = changeresult[fname]
+        if fname in declinematerials:
+            fd["pattern"][0] = " " + fd["pattern"][0][1] + " "
+            fd["pattern"][2] = " " + fd["pattern"][2][1] + " "
+        if fname in sawmaterials:
+            fd["pattern"][1] = fd["pattern"][1][0:2] + fd["pattern"][0][2]
+        if fname not in blacklist:
+            if "key" in fd:
+                for i in fd["key"]:
+                    for j in range(min(len(original), len(replacement))):
+                        if fd["key"][i] == original[j]:
+                            fd["key"][i] = replacement[j]
+            elif "ingredients" in fd:
+                for i in range(len(fd["ingredients"])):
+                    for j in range(min(len(original), len(replacement))):
+                        if fd["ingredients"][i] == original[j]:
+                            fd["ingredients"][i] = replacement[j]
+        fl = open(join(dname, fname), "w")
+        fl.write(dumps(fd, indent=2))
+        fl.close()
+
+
+def dealpath(dname, act):
+    for i in listdir(dname):
+        if isdir(join(dname, i)):
+            dealpath(join(dname, i), act or i == "recipes")
+        elif act:
+            handle(dname, i)
+
+
+dealpath(argv[1], False)
